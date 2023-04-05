351363cb4e5f5efc96ba9eb39ecd03c2edcd227a
Gentle-Programmer <annatataruch@gmail.com>
2023-04-05 09:11:28 +0000

    Add h2 to motivate users to fill out the quiz

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("currentYear").textContent = new Date().getFullYear();
+});
+
+const quizData = [
+{
+    question:""
+}
+
+]
