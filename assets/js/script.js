28ce9447c626f04bf18948dbe246da4eca9d191a
Gentle-Programmer <annatataruch@gmail.com>
2023-04-05 23:09:47 +0000

    Find partial fix to answer handler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-<script>
+// 
 
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("currentYear").textContent = new Date().getFullYear();
@@ -20,71 +20,71 @@ document.addEventListener("DOMContentLoaded", function () {
             infoModal.style.display = "none";
         }
     };
-});
+
     const quizData = [
     {
     question: "Q1: What is one of the most important factors in successful dog training?",
     options: ["Consistency", "Feeding Treats Constantly", "Changing Commands Frequently", "Dog's Age"],
     correct: 0,
-    image:"",
+    image: "./assets/images/Quiz_Q1.jpg",
     },
     {
     question: "Q2: How often should you train your dog?",
     options: ["Once a Month", "Once a Week", "Daily", "Only when the dog misbehaves"],
     correct: 2,
-    image:"",
+    image:"./assets/images/Quiz_Q2.jpg",
     },
     {
     question: "Q3: What is the optimal lenght of a leash for comfortable walks in urban environment?",
     options: ["4 m", "0.5 m", "1 m", "2 m"],
     correct: 3,
-    image:"",
+    image: "./assets/images/Quiz_Q3.jpg",
     },
     {   
     question: "Q4: You want to continue the walk but your dog insists on sniffing a spot, what should you do?",
     options: ["Pull on the lead and drag him away from that spot", "Give out to him and tug on the lead twice for attention", "Use the attention cue and reward when your dog responds", "Call your dog's name and show him some tasty treats to encourage him to move away from the spot"],
     correct: 3,
-    image:"",
+    image: "./assets/images/Quiz_Q4.jpg",
     },
     {
     question: "Q5: Your 16 weeks old puppy meets a group of off lead dogs at play, what should you do?",
     options: ["Immediately let my puppy off lead and let him play as much as he wants", "Don't let your puppy interact with unknown dogs and quickly walk away", "Let my puppy say hello to a few selected calm dogs in 1on1 setting and walk away"],
     correct: 2,
-    image:"",
+    image: "./assets/images/Quiz_Q5.jpg",
     },
     {
     question: "Q6: We use marker word 'Yes!' to:",
     options: ["Tell the dog he is a good boy", "Highlight the exact moment he guest the correct answer", "To condition him that training is fun"],
     correct: 1,
-    image:"",
+    image: "./assets/images/Quiz_Q6.jpg",
     },
     {
     question: "Q7: Your dog sits on command perfectly at home but he won't do it outside, why?",
     options: ["The dog is just stubborn and you need to insist on him obeying your command", "The command is too weak in comparison to distraction level", "He is more excited when outside and you need to repeat the work a few times until he hears you"],
     correct: 1,
-    image:"",
+    image: "./assets/images/Quiz_Q7.jpg",
     },
     {
     question: "Q8: What is more important?",
     options: ["Reward progression towards the final behaviour", "Punish bad behaviour", "Insist on your dog doing the exercise even if you need to put him in position a couple of times"],
     correct: 0,
-    image:"",
+    image: "./assets/images/Quiz_Q8.jpg",
     },
     {
     question: "Q9: Your dog barks and lunges towards dogs most dogs he meets on a walk, what should you do when it happens?",
     options: ["Pull your dog to you immediately when you see a dog approaching and either wait until they pass or pull your dog away making sure he knows this behaviour is 'bold'", "Do nothing, dogs bark, it's normal", "Shorten the lead appropriately ensuring it's not tight and walk away using appropriate command"],
     correct: 2,
-    image:"",
+    image: "./assets/images/Quiz_Q9.jpg",
     },
     {
     question: "Q10: What is the most time effective strategy for resolving nuisance behaviours such as begging for food or chewing on furniture?",
     options: ["Maintain tight management, have a solid daily routine and reward the opposite behaviour", "Be very consistent in telling your dog he is 'bold' for doing the wrong behaviour until he gets it", "Keep your dog in a crate until he gets over it"],
     correct: 0,
-    image:"",  
+    image: "./assets/images/Quiz_Q10.jpg",  
     },
   ]; 
 
-
+// 
 
     const startBtn = document.getElementById("startBtn");
     const quizContainer = document.getElementById("quizContainer");
@@ -131,15 +131,14 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function handleAnswer(selected) {
-    const correctAnswer = quizdata[currentQuestion].correct;
+    const correctAnswer = quizData[currentQuestion] ? ".correct" : ".incorrect";
     if (selected === correctAnswer) {
         score++;
     }
 
     if (currentQuestion === quizData.length - 1) {
         displayResults();
-    }
-    else {
+    } else {
         currentQuestion++;
         displayQuestion();
     }
@@ -176,5 +175,4 @@ document.addEventListener("DOMContentLoaded", function () {
            currentQuestion++;
            displayQuestion();
         };
-
-</script>
+});
